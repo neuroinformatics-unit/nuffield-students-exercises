3a29d3a3b19fdf39930b0e157009ff289d1e8d72
training <training@Adams-MacBook-Pro.local>
2024-08-08 16:32:49 +0100

    the project runs and functions correctly - docstrings and typing added

--- ucl_project/deborah_project.py
+++ ucl_project/deborah_project.py
@@ -4,36 +4,17 @@ import skimage as ski
 import tifffile as tif
 from tqdm import tqdm
 
-#first, I will import the video (the derotated image) (using tifffile)
+#first, I will import the video (the derotated video clip) (using tifffile)
 video = tif.imread('/Users/training/Downloads/derotated/derotated.tif')
-print(np.shape(video))
-#afterwards, I will define FOUR functions, A, B and C:
-
-#FUNCTION A: at each frame, i.e. identifying a lower and higher threshold 
-#based on the histogram of a given image (this I think increases the noise but makes the image sharper)
+#afterwards, I will define THREE functions, A, B and C:
+#FUNCTION A: at each frame, i.e. identifying a lower and higher threshold based on the histogram of a given image 
+#(this I think increases the noise but makes the image sharper)
 def single_thresholding_for_each_image( #defining the function
-        video, contrast_level): #assigning two inputs to the function
-    arr = []
-    for number in tqdm(range(0, 8258)): #creating a for loop to iterate the thresholds to each frame
-        t_min, t_max = find_thresholds(video[number, :, :], contrast_level) #assigning the min and max threshold values to a variable
-        first_thresholded_image = np.where(video[number, :, :] < t_min, 0, video[number, :, :]) #adding the lower threshold to the frame
-        second_thresholded_image= np.where(first_thresholded_image > t_max, 256, first_thresholded_image) #adding the upper threshold to the frame
-        arr.append(second_thresholded_image)
-    return np.asarray(arr) #assigning an output which is the new created video
-"""This function will apply two unique thresholds to each frame of a video
-and return a new thresholded video ()
-...............
-"""
-
-#FUNCTION B: or all the video, i.e. identifying the two thresholds based on the overall histogram.
-def double_thresholding_for_whole_video(
-        video, contrast_level):
-    t_min, t_max = find_thresholds(video, contrast_level)
-    first_thresholded_video = np.where(video < t_min, 0, video)
-    second_thresholded_video = np.where(first_thresholded_video > t_max, 256, first_thresholded_video)
-    return second_thresholded_video
-"""This function will apply two thresholds to a single video (first_thresholded_video)
-and return a new video (second_thresholded_video)
+        video: np.ndarray, 
+        contrast_level: int
+) -> np.ndarray: #assigning two inputs to the function
+    """This function will apply two unique thresholds to each frame of a video
+    and return a new thresholded video (arr)
 
     Parameters
     ----------
@@ -41,23 +22,52 @@ and return a new video (second_thresholded_video)
         The input video.
     contrast_level : int
         The input contrast level
- 
+
     Returns
     -------
-    second_thresholded_video
+    arr : np.ndarray
         The new output video.
-"""
+    """
+    arr = []
+    for number in tqdm(range(0, 8258)): #creating a 'for loop' to iterate the thresholds to each frame
+        t_min, t_max = find_thresholds(video[number, :, :], contrast_level) #assigning the min. and max. threshold values to two variables
+        first_thresholded_image = np.where(video[number, :, :] < t_min, 0, video[number, :, :]) #adding the lower threshold to the frame
+        second_thresholded_image= np.where(first_thresholded_image > t_max, 256, first_thresholded_image) #adding the upper threshold to the frame
+        arr.append(second_thresholded_image) #add each thresholded frame to the list, one after the other
+    return np.asarray(arr) #assigning an output which is the new created video
+
+#FUNCTION B: or all the video, i.e. identifying the two thresholds based on the overall histogram.
+def double_thresholding_for_whole_video(
+        video: np.ndarray, 
+        contrast_level: int
+) -> np.ndarray:
+    """This function will apply two thresholds to a single video (first_thresholded_video)
+    and return a new video (second_thresholded_video)
+
+    Parameters
+    ----------
+    video : np.ndarray
+        The input video.
+    contrast_level : int
+        The input contrast level
+
+    Returns
+    -------
+    second_thresholded_video : np.ndarray
+        The new output video.
+    """
+    t_min, t_max = find_thresholds(video, contrast_level)
+    first_thresholded_video = np.where(video < t_min, 0, video)
+    second_thresholded_video = np.where(first_thresholded_video > t_max, 256, first_thresholded_video)
+    return second_thresholded_video
 
 #FUNCTION C:
 def find_thresholds(
-        array, contrast_level):
-    t_min, t_max = np.percentile(
-        array, (contrast_level, 100 - contrast_level)
-    )
-
-    return t_min, t_max
-"""This function will identify two thresholds based on an input array and contrast level
-and return the two threshold values (t_min, t_max) 
+        array: np.ndarray,
+        contrast_level: int
+) -> np.ndarray:
+    """This function will identify two thresholds based on an input array and contrast level
+    and return the two threshold values (t_min, t_max) 
 
     Parameters
     ----------
@@ -65,12 +75,14 @@ and return the two threshold values (t_min, t_max)
         The input video.
     contrast_level : int
         The input contrast level
- 
+
     Returns
     -------
-    t_min, t_max
+    t_min, t_max : int
         The new output thresholds.
-"""
+    """
+    t_min, t_max = np.percentile(array, (contrast_level, 100 - contrast_level))
+    return t_min, t_max
 
 #After, use the two functions to generate two different videos with different contrasts
 video_contrast_single = single_thresholding_for_each_image(video, 0.9)
